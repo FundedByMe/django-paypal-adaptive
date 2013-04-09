896e1a8ecf4dde0a59fe93899f7df881d097d95f
Anton Agestam <msn@antonagestam.se>
2013-04-09 18:13:04 +0200

    removed

--- paypaladaptive/api/__init__.py
+++ paypaladaptive/api/__init__.py
@@ -1,3 +1,1 @@
-__author__ = 'antonagestam'
-
 from endpoints import *
--- paypaladaptive/signals.py
+++ paypaladaptive/signals.py
@@ -1,5 +1,3 @@
-__author__ = 'antonagestam'
-
 from django.dispatch import Signal
 
 received_preapproval = Signal(providing_args=['preapproval'])
