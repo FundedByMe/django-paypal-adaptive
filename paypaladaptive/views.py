eb495d1f4fb92a9ab588f73d549d19f1cee9359b
Anton Agestam <msn@antonagestam.se>
2013-04-09 13:32:32 +0200

    cleanup, comments and a TODO

--- paypaladaptive/views.py
+++ paypaladaptive/views.py
@@ -1,39 +1,46 @@
-'''
+"""
 Paypal Adaptive Payments supporting views
 
 Created on Jun 13, 2011
 
 @author: greg
-'''
+"""
+
 from api import IpnError
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
-from django.http import HttpResponseForbidden, HttpResponseServerError, Http404, \
-    HttpResponse, HttpResponseBadRequest
+from django.http import (HttpResponseForbidden, HttpResponseServerError,
+                         Http404, HttpResponse, HttpResponseBadRequest)
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from models import Payment, Preapproval
+from django.shortcuts import get_object_or_404
 import api
 import logging
 import settings
+from django.utils.translation import ugettext_lazy as _
+from django.utils import simplejson as json
+
+# TODO: Remove fundedbyme-specific code
+from facepy import GraphAPI
+from fundedbyme.project.tasks import add_funds_to_project, add_backer_to_project, send_timeline_update, send_twitter_update
+from social_auth.models import UserSocialAuth
 
 logger = logging.getLogger(__name__)
 
 @login_required
 @transaction.autocommit
-def payment_cancel(request, payment_id, payment_secret_uuid, template="paypaladaptive/cancel.html"):
-    '''
-    Incoming cancellation from paypal
-    '''
+def payment_cancel(request, payment_id, payment_secret_uuid,
+                   template="paypaladaptive/cancel.html"):
+    """Handle incoming cancellation from paypal"""
+
     logger.debug( "Cancellation received for Payment %s" % payment_id)
-    
-    try:
-        payment = Payment.objects.get(id=payment_id, secret_uuid=payment_secret_uuid)
-    except ObjectDoesNotExist:
-        raise Http404
+
+    payment = get_object_or_404(Payment, id=payment_id,
+                                secret_uuid=payment_secret_uuid)
     
     if request.user != payment.purchaser:
         return HttpResponseForbidden("Unauthorized")
@@ -49,28 +56,32 @@ def payment_cancel(request, payment_id, payment_secret_uuid, template="paypalada
 
 @login_required
 @transaction.autocommit
-def payment_return(request, payment_id, payment_secret_uuid, template="paypaladaptive/return.html"):
-    '''
-    Incoming return from paypal process (note this is a return to the site, not a returned payment)
-    '''
-    logger.debug( "Return received for Payment %s" % payment_id)
-    
-    try:
-        payment = Payment.objects.get(id=payment_id, secret_uuid=payment_secret_uuid)
-    except ObjectDoesNotExist:
-        raise Http404
-    
+def payment_return(request, payment_id, payment_secret_uuid,
+                   template="paypaladaptive/return.html"):
+    """
+    Incoming return from paypal process (note this is a return to the site, not
+    a returned payment)
+    """
+
+    logger.debug("Return received for Payment %s" % payment_id)
+
+    payment = get_object_or_404(Payment, id=payment_id,
+                                secret_uuid=payment_secret_uuid)
+
     if request.user != payment.purchaser:
         return HttpResponseForbidden("Unauthorized")
     
     if payment.status not in ['created', 'completed']:
-        payment.status_detail = 'Expected status to be created or completed, not %s - duplicate transaction?' % payment.status
+        payment.status_detail = _(u"Expected status to be created or "
+                                  u"completed, not %s - duplicate "
+                                  u"transaction?") % payment.status
         payment.status = 'error'
         payment.save()
         return HttpResponseServerError('Unexpected error')
 
     elif payment_secret_uuid != payment.secret_uuid:
-        payment.status_detail = 'BuyReturn secret "%s" did not match' % payment_secret_uuid
+        payment.status_detail = _(u"BuyReturn secret \"%s\" did not match")\
+                                % payment_secret_uuid
         payment.status = 'error'
         payment.save()
         return HttpResponseServerError('Unexpected error')
@@ -91,17 +102,14 @@ def payment_return(request, payment_id, payment_secret_uuid, template="paypalada
 
 @login_required
 @transaction.autocommit
-def preapproval_cancel(request, id, template="paypaladaptive/cancel.html"):
-    '''
-    Incoming preapproval cancellation from paypal
-    '''
-    logger.debug( "Cancellation received for Preapproval %s" % id)
-    
-    try:
-        preapproval = Preapproval.objects.get(id=id)
-    except ObjectDoesNotExist:
-        raise Http404
-    
+def preapproval_cancel(request, preapproval_id,
+                       template="paypaladaptive/cancel.html"):
+    """Incoming preapproval cancellation from paypal"""
+
+    logger.debug("Cancellation received for Preapproval %s" % preapproval_id)
+
+    preapproval = get_object_or_404(Preapproval, id=preapproval_id)
+
     if request.user != preapproval.purchaser:
         return HttpResponseForbidden("Unauthorized")
     
@@ -114,34 +122,33 @@ def preapproval_cancel(request, id, template="paypaladaptive/cancel.html"):
         
     return render_to_response(template, template_vars, context)
 
-from django.utils import simplejson as json
-from facepy import GraphAPI
-from fundedbyme.project.tasks import add_funds_to_project, add_backer_to_project, send_timeline_update, send_twitter_update
-from social_auth.models import UserSocialAuth
 @login_required
 @transaction.autocommit
-def preapproval_return(request, id, secret_uuid, template="paypaladaptive/return.html"):
-    '''
-    Incoming return from paypal process (note this is a return to the site, not a returned payment)
-    '''
-    logger.debug( "Return received for Payment %s" % id)
-    
-    try:
-        preapproval = Preapproval.objects.get(id=id)
-    except ObjectDoesNotExist:
-        raise Http404
-    
+def preapproval_return(request, preapproval_id, secret_uuid,
+                       template="paypaladaptive/return.html"):
+    """
+    Incoming return from paypal process (note this is a return to the site,
+    not a returned payment)
+    """
+
+    logger.debug("Return received for Payment %s" % preapproval_id)
+
+    preapproval = get_object_or_404(Preapproval, id=preapproval_id)
+
     if request.user != preapproval.purchaser:
         return HttpResponseForbidden("Unauthorized")
     
     if preapproval.status not in ['created', 'completed']:
-        preapproval.status_detail = 'Expected status to be created or completed, not %s - duplicate transaction?' % preapproval.status
+        preapproval.status_detail = _(u"Expected status to be created or"
+                                      u" completed, not %s - duplicate"
+                                      u" transaction?") % preapproval.status
         preapproval.status = 'error'
         preapproval.save()
         return HttpResponseServerError('Unexpected error')
 
     elif secret_uuid != preapproval.secret_uuid:
-        preapproval.status_detail = 'BuyReturn secret "%s" did not match' % secret_uuid
+        preapproval.status_detail = _(u"BuyReturn secret \"%s\" did not"
+                                      u" match") % secret_uuid
         preapproval.status = 'error'
         preapproval.save()
         return HttpResponseServerError('Unexpected error')
@@ -149,6 +156,8 @@ def preapproval_return(request, id, secret_uuid, template="paypaladaptive/return
     if preapproval.status != 'completed':
         preapproval.status = 'returned'
         preapproval.save()
+
+        # TODO: this code should live in the fundedbyme project:
         json_object = json.loads(preapproval.debug_request)
         for majorkey, subdict in json_object.iteritems():
             if majorkey == "project":
@@ -167,7 +176,8 @@ def preapproval_return(request, id, secret_uuid, template="paypaladaptive/return
         pass
 
     context = RequestContext(request)
-    template_vars = {"is_embedded": settings.USE_EMBEDDED,"preapproval":preapproval,}
+    template_vars = {"is_embedded": settings.USE_EMBEDDED,
+                     "preapproval": preapproval, }
 
     return render_to_response(template, template_vars, context)
 
