e748042f760f806eec08f97a27b35968fe0647c9
Anton Agestam <msn@antonagestam.se>
2013-04-19 12:17:53 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,10 @@ setup(
     install_requires=[
         'Django>=1.4.3',
         'python-dateutil==2.1',
-        'python-money==1.0',
+        'python-money',
     ],
     dependency_links=[
-        '-e hg+https://bitbucket.org/acoobe/python-money/@edf852242ce422a7320c'
-        '5fc95ceb01c0d38e7647#egg=python_money-1.0'
+        '-e hg+https://bitbucket.org/acoobe/python-money/@edf852242ce422a7320c5fc95ceb01c0d38e7647#egg=python_money'
     ],
     description = 'A pluggable Django application for integrating PayPal'
                   'Adaptive Payments',
@@ -32,3 +31,4 @@ setup(
         "Topic :: Software Development"
     ],
 )
+
