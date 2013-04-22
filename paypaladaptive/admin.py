7d9d98878c9b207909737443cf6b652b278ee9d1
Anton Agestam <msn@antonagestam.se>
2013-04-22 14:21:17 +0200

    let implementer register admins

--- paypaladaptive/admin.py
+++ paypaladaptive/admin.py
@@ -4,6 +4,12 @@ from models import Payment, Preapproval, Refund
 class PreapprovalAdmin(admin.ModelAdmin):
     list_display = ('preapproval_key', 'valid_until_date', 'status')
 
-admin.site.register(Payment)
-admin.site.register(Preapproval, PreapprovalAdmin)
-admin.site.register(Refund)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+class RefundAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Payment)
+# admin.site.register(Preapproval, PreapprovalAdmin)
+# admin.site.register(Refund)
