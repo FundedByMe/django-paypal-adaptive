af204e655bcfe944ffee0156b01d9fc3c197b52d
Rebecca Meritz <rebecca@meritz.com>
2013-04-24 10:19:23 +0200

    Add paypaladaptive tests

--- paypaladaptive/tests/__init__.py
+++ paypaladaptive/tests/__init__.py
@@ -1,1 +1,1 @@
-from models import *
+from tests import *
---
+++ paypaladaptive/tests/tests.py
@@ -0,0 +1,176 @@
+import json
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
+
+from money import Money
+from paypaladaptive import settings
+from paypaladaptive.models import Preapproval, Payment
+from paypaladaptive.api import Receiver, ReceiverList
+from mock import patch
+
+
+class MockUrlRequest(object):
+    def run(self, url, data=None, headers=None):
+        self._assert_valid_url(url)
+        self._assert_valid_data(json.loads(data))
+        self._assert_valid_headers(headers)
+        return self._response()
+
+    def _response(self):
+        """ Define JSON string returned by paypal"""
+        raise RuntimeError
+
+    def _assert_valid_url(self, url):
+        valid_url = "%s%s" % (settings.PAYPAL_ENDPOINT, self._endpoint_name())
+        assert url == valid_url
+
+    def _endpoint_name(self):
+        """"Name of API call to paypal"""
+        raise RuntimeError
+
+    def _assert_valid_data(self, data):
+        self._assert_valid_return_url(data["returnUrl"])
+        self._assert_valid_cancel_url(data["cancelUrl"])
+        assert data["requestEnvelope"] == {"errorLanguage": "en_US"}
+
+    def _assert_valid_headers(self, headers):
+        self._assert_valid_header(headers, 'X-PAYPAL-SECURITY-SIGNATURE',
+                                  settings.PAYPAL_SIGNATURE)
+        self._assert_valid_header(headers, 'X-PAYPAL-REQUEST-DATA-FORMAT',
+                                  'JSON')
+        self._assert_valid_header(headers, 'X-PAYPAL-APPLICATION-ID',
+                                  settings.PAYPAL_APPLICATION_ID)
+        self._assert_valid_header(headers, 'X-PAYPAL-SECURITY-USERID',
+                                  settings.PAYPAL_USERID)
+        self._assert_valid_header(headers, 'X-PAYPAL-SECURITY-PASSWORD',
+                                  settings.PAYPAL_PASSWORD)
+        self._assert_valid_header(headers, 'X-PAYPAL-RESPONSE-DATA-FORMAT',
+                                  'JSON')
+
+    def _assert_valid_header(self, headers, name, value):
+        assert headers[name] == value
+
+    def _assert_valid_reversed_url(self, url, kwargs, template_name):
+        """"Helper function for return and cancel urls"""
+        current_site = Site.objects.get_current()
+        return_url = reverse(template_name, kwargs=kwargs)
+        assert url == "http://%s%s" % (current_site, return_url)
+
+
+class MockUrlRequestPreapproval(MockUrlRequest):
+
+    def _response(self):
+        return """{\"responseEnvelope\":
+                {\"timestamp\": \"2013-04-23T02:03:21.737-07:00\",
+                 \"ack\": \"Success\",
+                 \"correlationId\": \"877ca0ec3b8db\",
+                 \"build\": \"5710487\"},
+                \"preapprovalKey\": \"PA-8X048594NU8990615\"}"""
+
+    def _endpoint_name(self):
+        return "Preapproval"
+
+    def _assert_valid_data(self, data):
+        super(MockUrlRequestPreapproval, self)._assert_valid_data(data)
+        assert data["currencyCode"] == str(self.preapproval().currency)
+        assert data["maxNumberOfPayments"] == 1
+        assert data["maxNumberOfPaymentsPerPeriod"] == 1
+        assert data["endingDate"] is not None
+        assert data["startingDate"] is not None
+        assert data["pinType"] == "NOT_REQUIRED"
+        assert data["maxTotalAmountOfAllPayments"] == self.preapproval().amount
+
+    def _assert_valid_return_url(self, url):
+        kwargs = {'preapproval_id': self.preapproval().id,
+                  'secret_uuid': self.preapproval().secret_uuid}
+        self._assert_valid_reversed_url(url, kwargs,
+                                        'paypal-adaptive-preapproval-return')
+
+    def _assert_valid_cancel_url(self, url):
+        kwargs = {'preapproval_id': self.preapproval().id}
+        self._assert_valid_reversed_url(url, kwargs,
+                                        'paypal-adaptive-preapproval-cancel')
+
+
+class MockUrlRequestPayment(MockUrlRequest):
+    def _response(self):
+        return """{\"responseEnvelope\":
+                {\"timestamp\":\"2013-04-23T07:45:36.456-07:00\",
+                 \"ack\":\"Success\",
+                 \"correlationId\":\"0d9ee9c8aab09\",
+                 \"build\":\"5710487\"},
+                \"payKey\":\"AP-7KL74713BP0955948\",
+                \"paymentExecStatus\":\"CREATED\"}"""
+
+    def _endpoint_name(self):
+        return "Pay"
+
+    def _assert_valid_return_url(self, url):
+        kwargs = {'payment_id': self.payment().id,
+                  'secret_uuid': self.payment().secret_uuid}
+        self._assert_valid_reversed_url(url, kwargs,
+                                        'paypal-adaptive-payment-return')
+
+    def _assert_valid_cancel_url(self, url):
+        self._assert_valid_reversed_url(url, {'payment_id': self.payment().id},
+                                        'paypal-adaptive-payment-cancel')
+
+    def _assert_valid_data(self, data):
+        super(MockUrlRequestPayment, self)._assert_valid_data(data)
+        assert data["currencyCode"] == str(self.payment().currency)
+        assert data["actionType"] == "PAY"
+        self._assert_valid_recievers_list(data["receiverList"]["receiver"])
+
+    def _assert_valid_recievers_list(self, reciever_list):
+        assert reciever_list == self.recievers().to_dict()
+
+
+class AdaptiveTests(TestCase):
+
+    def setUp(self):
+
+        self.preapproval = Preapproval()
+        self.preapproval.money = Money(100, "SEK")
+        self.preapproval.save()
+        self.preapproval = Preapproval.objects.all()[0]
+
+        self.payment = Payment()
+        self.payment.money = Money(100, "USD")
+        self.payment.save()
+        self.payment = Payment.objects.all()[0]
+
+        george = Receiver(amount=10, email="msn-facilitator@antonagestam.se",
+                          primary=False)
+        allen = Receiver(amount=90, email="mrbuyer@antonagestam.se",
+                         primary=True)
+        self.recievers = ReceiverList([george, allen])
+
+    @patch("paypaladaptive.api.endpoints.UrlRequest",
+           MockUrlRequestPreapproval)
+    def test_preapproval(self):
+        MockUrlRequestPreapproval.preapproval = (
+            classmethod(lambda cls: self.preapproval))
+        self.assertTrue(self.preapproval.process())
+        self.preapproval = Preapproval.objects.get(pk=self.preapproval.pk)
+        self.assertNotEqual(self.preapproval.preapproval_key, "")
+        self.assertEqual(self.preapproval.status, "created")
+
+    @patch("paypaladaptive.api.endpoints.UrlRequest",
+           MockUrlRequestPayment)
+    def test_payment(self):
+        MockUrlRequestPayment.payment = classmethod(lambda cls: self.payment)
+        MockUrlRequestPayment.recievers = classmethod(
+            lambda cls: self.recievers)
+        self.assertTrue(self.payment.process(self.recievers))
+        self.payment = Payment.objects.get(pk=self.payment.id)
+        self.assertTrue(self.payment.status, "created")
+        self.assertNotEqual(self.payment.pay_key, "")
+
+    # Doesn't work
+    def test_payment_from_preapproval(self):
+        self.preapproval.process()
+        preapproval = Preapproval.objects.get(pk=self.preapproval.pk)
+        self.assertTrue(self.payment.process(self.recievers,
+                                             preapproval=preapproval))
